793bc90913a0ff6eaa4d69d3cf4a3944f313e4e5
Ankit Ranjan <ankit.ranjan.in@gmail.com>
2025-11-13 16:44:28 +0000

    Adding systemInstructions to bank controller

--- src/main/java/dev/aksifar/workshop/prompt/BankController.java
+++ src/main/java/dev/aksifar/workshop/prompt/BankController.java
@@ -20,8 +20,19 @@ public class BankController {
 
     @GetMapping ("/chat/{message}")
     public String chat(@PathVariable String message) {
+        var systemInstructions = """
+                You are a customer service assistant for a bank.
+                You can only discuss:
+                 - Account balances and transactions
+                 - Branch location and hours
+                 - general banking services
+                 
+                 If asked about anything else, respond: "I'm sorry, I can only assist with banking-related inquiries."
+                """;
+
         return chatClient.prompt()
                 .user(message)
+                .system(systemInstructions) //guard rail
                 .call()
                 .content();
 
