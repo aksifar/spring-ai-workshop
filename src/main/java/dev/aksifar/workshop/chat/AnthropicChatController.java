b82d796fc0c5210cbbdcc38226a3118b9a4f5c26
Ankit Ranjan <ankit.ranjan.in@gmail.com>
2025-11-13 13:25:07 +0000

    Initial Commit: open AI and anthroic chat clients

---
+++ src/main/java/dev/aksifar/workshop/chat/AnthropicChatController.java
@@ -0,0 +1,41 @@
+package dev.aksifar.workshop.chat;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import reactor.core.publisher.Flux;
+
+public class AnthropicChatController {
+
+    private final ChatClient chatClient;
+
+    public AnthropicChatController(@Qualifier("anthropicChatClient") ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+
+    @GetMapping("/chat1")
+    public String chat() {
+        return chatClient.prompt()
+                .user("Tell me an interesting fact about Java.")
+                .call()
+                .content();
+    }
+
+    @GetMapping("/stream1")
+    public Flux<String> stream() {
+        return chatClient.prompt()
+                .user("tell me in under 10 words the best tourist attraction in London")
+                .stream()
+                .content();
+    }
+
+    @GetMapping("/joke1")
+    public ChatResponse joke() {
+        return chatClient.prompt()
+                .user("Tell me a programming joke.")
+                .call()
+                .chatResponse();
+    }
+}
