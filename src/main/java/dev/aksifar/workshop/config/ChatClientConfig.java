ded776d2793b37cef0cedb3f330652719eb1c380
Ankit Ranjan <ankit.ranjan.in@gmail.com>
2025-11-14 17:36:52 +0000

    Chat with memory

--- src/main/java/dev/aksifar/workshop/config/ChatClientConfig.java
+++ src/main/java/dev/aksifar/workshop/config/ChatClientConfig.java
@@ -2,6 +2,8 @@ package dev.aksifar.workshop.config;
 
 import org.springframework.ai.anthropic.AnthropicChatModel;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +11,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ChatClientConfig {
 
+    private final ChatMemory chatMemory;
+
+    public ChatClientConfig(ChatMemory chatMemory) {
+        this.chatMemory = chatMemory;
+    }
+
+
     @Bean
     public ChatClient openAIChatClient(OpenAiChatModel chatModel) {
         return ChatClient.create(chatModel);
+    }
+
+    @Bean
+    public ChatClient openAIChatClientWithMemory(OpenAiChatModel chatModel) { //with memory
+        ChatClient.Builder builder = ChatClient.builder(chatModel);
+
+        return builder
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
     }
 
     @Bean
---
+++ src/main/java/dev/aksifar/workshop/memory/MemoryController.java
@@ -0,0 +1,25 @@
+package dev.aksifar.workshop.memory;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemoryController {
+
+    private final ChatClient chatClient;
+
+    public MemoryController(@Qualifier("openAIChatClientWithMemory") ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/memory-chat")
+    public String memoryChat(@RequestParam String message) {
+        return chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
+    }
+}
