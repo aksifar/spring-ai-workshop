c2696a55ccdebd4feeb177ebe667f3d0c2407784
Ankit Ranjan <ankit.ranjan.in@gmail.com>
2025-11-16 12:19:15 +0000

    tools for LLM

--- src/main/java/dev/aksifar/workshop/tools/datetime/DateTimeTools.java
+++ src/main/java/dev/aksifar/workshop/tools/datetime/DateTimeTools.java
@@ -4,11 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Slf4j
-@Component
+@Service
 public class DateTimeTools {
 
     @Tool(description = "Get the current date and time in the user's timezone.")
--- src/main/java/dev/aksifar/workshop/util/Constants.java
+++ src/main/java/dev/aksifar/workshop/util/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
 
     public static final String SLASH = "/";
     public static final String VECTOR_STORE_NAME = "vectorStore.json";
+    public static final String BASE_URL = "https://api.weather.gov";
 
     private Constants() throws IllegalAccessException {
         throw new IllegalAccessException("Utility class, not to be instantiated");
